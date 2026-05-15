a2664fc08026cbc4a4a26eb488d9f62ef97315c7
Shreyas <shreyas@student.tudelft.nl>
2026-05-15 23:29:52 +0200

    First look at a psi implementation

--- bloom_filter.hpp
+++ bloom_filter.hpp
@@ -15,5 +15,6 @@ BloomFilter bf_init(size_t m, size_t k);
 size_t bf_hash(const std::string& s, size_t i, size_t m);
 void bf_add(BloomFilter& bf, const std::string& s);
 bool bf_check(const BloomFilter& bf, const std::string& s);
+int phi_bit(const std::string& s, size_t j);
 
 #endif
--- server/protocols.cpp
+++ server/protocols.cpp
@@ -6,6 +6,19 @@
 #include <vector>
 #include <string>
 
+static std::vector<mpz_class> xor_bf_with_phi(GM& gm, const GMPublicKey& pk,
+    const std::vector<mpz_class>& encrypted_bf, const std::string& si, size_t m) {
+    std::vector<mpz_class> result(K);
+    for (size_t j = 0; j < K; j++) {
+        size_t index = bf_hash(si, j, m);
+        int phi = phi_bit(si, j);
+        result[j] = phi
+            ? gm.homomorphic_xor(pk, encrypted_bf[index], gm.encrypt_bit(pk, 1))
+            : gm.rerandomize(pk, encrypted_bf[index]);
+    }
+    return result;
+}
+
 void server_psi_ca(sock_t fd, const std::vector<std::string>& X, const GMPublicKey& pk, uint32_t v) {
     uint32_t m32 = 0;
     recv_all(fd, &m32, sizeof(m32));
@@ -26,7 +39,21 @@ void server_psi_ca(sock_t fd, const std::vector<std::string>& X, const GMPublicK
     }
 }
 
-void server_psi(sock_t fd, const std::vector<std::string>& X, const GMPublicKey& pk, uint32_t v) {}
+void server_psi(sock_t fd, const std::vector<std::string>& X, const GMPublicKey& pk, uint32_t v) {
+    uint32_t m32 = 0;
+    recv_all(fd, &m32, sizeof(m32));
+    size_t m = static_cast<size_t>(m32);
+
+    std::vector<mpz_class> encrypted_bf(m);
+    for (size_t i = 0; i < m; i++)
+        recv_mpz(fd, encrypted_bf[i]);
+
+    GM gm;
+    for (const auto& si : X) {
+        for (const auto& c : xor_bf_with_phi(gm, pk, encrypted_bf, si, m))
+            send_mpz(fd, c);
+    }
+}
 
 void server_apsi_ca(sock_t fd, const std::vector<std::string>& X, const GMPublicKey& pk, uint32_t v) {}
 
